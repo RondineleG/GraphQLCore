de84a5fc37ef31963d07db4fecbea30a5bcfaf69
Rondinele <rondineleg@edu.univali.br>
2019-11-20 08:36:56 -0300

    Criando clasee AppSchema e dentro do construtor, injetamos o IdependencyResolver

---
+++ GraphQLCore/GraphQL/GraphQLSchema/AppSchema.cs
@@ -0,0 +1,13 @@
+﻿using GraphQL;
+using GraphQL.Types;
+
+namespace GraphQLCore.GraphQL.GraphQLSchema
+{
+    public class AppSchema : Schema
+    {
+        public AppSchema(IDependencyResolver resolver) : base (resolver)
+        {
+                
+        }
+    }
+}
